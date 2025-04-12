37ebc6148a5d62c94792e94bbd02acfea293bd89
Dawid Nowacki <dawid.nowacki09@gmail.com>
2025-04-12 20:11:07 +0200

    Add files via upload

---
+++ buckbuddy/src/App.tsx
@@ -0,0 +1,14 @@
+import Exchanger from "./Exchanger"
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <h1>💸 BuckBuddy</h1>
+      </header>
+      <main>
+        <Exchanger />
+      </main>
+    </>
+  )
+}
